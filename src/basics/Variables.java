3aab1a74936b72a3630243f87d5eb86440e6b3f6
stefan <stefanbayne@gmail.com>
2022-08-05 22:03:38 -0400

    Initial commit.

---
+++ src/basics/Variables.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Variables {
+
+    /*
+    Let's begin by understanding the basics of java variables.
+     */
+    public static void main(String[] args) {
+        int x = 30;
+        System.out.println("Hooray, my first Java app at age " + x);
+
+        for(int i = 1; i <= x; ++i) {
+            System.out.println("This is a for loop of i to x which is 30: " + i);
+        }
+
+
+
+    }
+}
