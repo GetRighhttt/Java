b01a4d5b004410a9256533979ec34cc293faecf7
stefan <stefanbayne@gmail.com>
2022-08-05 22:30:25 -0400

    First primitive type commit.

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class DataTypes {
+
+    /*
+    Let's begin by understanding the basics of java variables.
+
+    In Java, we have primitive types and reference types.
+     */
+    public static void main(String[] args) {
+        /*
+        Here is how we declare variables in Java.
+         */
+        byte theByte = -20; // byte value
+        short theShort = 20; // short value
+        int thisNumber = 45; // integer value
+        String newString = "This is a string."; // string value
+        Boolean isTrue = true; // Boolean value
+        double thisDouble = 50.000; // Double value
+        float thisFloat = 20.0F; // Float value
+        long thisLong = 20L; // Long value
+
+
+
+    }
+}
