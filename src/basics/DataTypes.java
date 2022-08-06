de80e770fb0a25765d10fad9785bb38d0026a78d
stefan <stefanbayne@gmail.com>
2022-08-06 11:30:18 -0400

    primitive types.

--- src/basics/DataTypes.java
+++ src/basics/DataTypes.java
@@ -3,23 +3,39 @@ package basics;
 public class DataTypes {
 
     /*
-    Let's begin by understanding the basics of java variables.
+    Let's begin by understanding the basics of java variables:
+
+    variable name can only be declared certain ways.
 
     In Java, we have primitive types and reference types.
      */
     public static void main(String[] args) {
         /*
-        Here is how we declare variables in Java.
+        Here is how we declare variables in Java
+        with an example of primitive data types.
          */
-        byte theByte = -20; // byte value
-        short theShort = 20; // short value
-        int thisNumber = 45; // integer value
-        String newString = "This is a string."; // string value
-        Boolean isTrue = true; // Boolean value
-        double thisDouble = 50.000; // Double value
-        float thisFloat = 20.0F; // Float value
-        long thisLong = 20L; // Long value
+        byte theByte = -128; // byte value stores whole numbers from -128 -> 127
+        short theShort = -32_000; // short value numbers = -32768 -> 32767
+        int thisNumber = 78_838_334; // integer value = -2 bill -> 2 bill
+        long thisLong = 16_513_151_681_351L; // Long value = over trillions
+        float thisFloat = 20.0F; // Float value = fractional 6-7 digits
+        double thisDouble = 50.000; // Double value = fractional values 15 digits
+        char newChar = 'C'; // character value
+        String newString = "This is a string."; // string = a sequence of characters
+        Boolean isTrue = true; // Boolean stores true or false
 
+        System.out.println(theByte);
+        System.out.println(theShort);
+        System.out.println(thisNumber);
+        System.out.println(thisLong);
+        System.out.println(thisFloat);
+        System.out.println(thisDouble);
+        System.out.println(newChar);
+        System.out.println(newString);
+        System.out.println(isTrue);
+        /*
+        Here is an example of reference variables.
+         */
 
 
     }
