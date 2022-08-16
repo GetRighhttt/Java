1972278f48f41a90b146ade61e779a28be658c76
stefan <stefanbayne@gmail.com>
2022-08-16 10:28:20 -0400

    Bried example of Declarative Approach.

---
+++ src/functionalprogramming/DeclarativeApproach.java
@@ -0,0 +1,20 @@
+package functionalprogramming;
+
+public class DeclarativeApproach {
+    public static void main(String[] args) {
+        /*
+        DeclarativeApproach or in other words, functional programming is essentially
+        just using streams with collections in Java.
+
+        streams() allows us to go into an abstract mode where we simply tell
+        it what we want.
+
+        so basically, if we wanted to do have the same output as we have for the
+        imperative approach, all we have to do is create a stream.
+
+        person.stream().filter(person -> FEMALE.equals(person.gender))
+        .foreach(System.out::println)
+         */
+
+    }
+}
