6eb30340aeecd6b6297b737f690d1800e0421481
stefan <stefanbayne@gmail.com>
2022-08-16 17:15:50 -0400

    More array methods added.

--- src/datastructures/ArraysExample.java
+++ src/datastructures/ArraysExample.java
@@ -8,6 +8,15 @@ public class ArraysExample {
     Arrays are probably the most important DataStructure in programming.
 
     They are used quite frequently, and especially in mobile development.
+
+    In Java and many other languages, Arrays are static, which means one you specify
+    its length, it's quite cumbersome to add on more space.
+
+    We have to know ahead of time how many items we want to store in an array.
+
+    Lookup O(1)
+    Insert O(n)
+    Delete O(n)
      */
     public static void main(String[] args) {
 
@@ -41,7 +50,7 @@ public class ArraysExample {
         myIntArray[7] = 89;
         myIntArray[8] = 74;
         myIntArray[9] = 60;
-        System.out.println(java.util.Arrays.toString(myIntArray));
+        System.out.println(Arrays.toString(myIntArray));
 
         // here is how we can loop through the elements of an array:
         for (int i = 0; i < myIntArray.length; i++) {
@@ -50,7 +59,7 @@ public class ArraysExample {
 
         // we can also specify elements in the array in one line.
         String[] newStringArray = {"Here", "we", "are."};
-        System.out.println(java.util.Arrays.toString(newStringArray));
+        System.out.println(Arrays.toString(newStringArray));
         /*
 
         /*
@@ -59,14 +68,14 @@ public class ArraysExample {
 
         // sort the array
         Arrays.sort(myIntArray);
-        System.out.println(java.util.Arrays.toString(myIntArray));
+        System.out.println(Arrays.toString(myIntArray));
 
         //copies the array, gives a new length
         System.out.println(Arrays.toString(
                 Arrays.copyOf(myIntArray, 20)));
 
         // copies a range of the array from index 0-6
-        System.out.println(java.util.Arrays.toString(
+        System.out.println(Arrays.toString(
                 Arrays.copyOfRange(myIntArray, 0, 6)));
 
         // deepEquals() returns true if the array is equal to the other array
@@ -76,6 +85,16 @@ public class ArraysExample {
         // will return the item index of 34
         int item = 34;
         System.out.println(Arrays.binarySearch(myIntArray, item));
+
+        // finds the first element and returns an optional integer.
+        System.out.println(Arrays.stream(myIntArray).findFirst());
+
+        // returns min and max of the array as OptionalInteger
+        System.out.println(Arrays.stream(myIntArray).min());
+        System.out.println(Arrays.stream(myIntArray).max());
+
+        // returns the average of all the integers in the array as OptionalDouble
+        System.out.println(Arrays.stream(myIntArray).average());
         /*
         Outputs:
 
@@ -84,6 +103,10 @@ public class ArraysExample {
         copyOfRange() = [1, 2, 6, 8, 26, 34]
         deepEquals() = false
         binarySearch(34) = 5
+        OptionalInt[1]
+        OptionalInt[1]
+        OptionalInt[89]
+        OptionalDouble[35.7]
          */
 
     }
