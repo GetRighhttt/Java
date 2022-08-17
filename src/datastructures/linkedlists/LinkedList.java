675b96dae3b93580e6e1316b5412eafa1d55b3ee
stefan <stefanbayne@gmail.com>
2022-08-17 10:14:19 -0400

    Finished the LinkedList.

--- src/datastructures/linkedlists/LinkedList.java
+++ src/datastructures/linkedlists/LinkedList.java
@@ -2,39 +2,31 @@ package datastructures.linkedlists;
 
 public class LinkedList {
 
-    // the node class is a part of the linked list class
-    // we put it inside so that we can have access to its fields
-    private static class Node {
-        private int value;
-        private Node next;
-
-        // constructor that always gives the node a value.
-        public Node(int value) {
-            this.value = value;
-        }
-    }
-
     // could call them head and tail but the java class refers to them
     // as first and last
     private Node first;
     private Node last;
 
+    // empty constructor
+    public LinkedList() {
+    }
+
+    // non-empty constructor
     public LinkedList(Node first, Node last) {
         this.first = first;
         this.last = last;
     }
 
-    /*
-    method to add last node onto the list.
+    /**
+     * Method to add last node onto the list.
+     * <p>
+     * We will create each method needed to deploy a personal linked list.
      */
     public void addLast(int item) {
         /*
         Step 1:
         This is how we first create a node object, and it should always
         store a value.
-
-        var keyword here is used because the type of the node is specified on the
-        end of the line already.
          */
         var node = new Node(item);
 
@@ -46,14 +38,142 @@ public class LinkedList {
         as soon as we add one, first is initialized.
          */
 
-        if(first == null) {
+        if (isListEmpty()) {
             // setting first and last node to point to new node
             first = node;
             last = node;
-        }
-        else { // linking last node to this node using "next" keyword
+        } else { // linking last node to this node using "next" keyword
             last.next = node;
             last = node;
+        }
+    }
+
+    /**
+     * Now we are going to create the addFirst method.
+     */
+    public void addFirst(int item) {
+        Node node = new Node(item);
+
+        // if entire list is empty, set first and last to new node.
+        if (isListEmpty()) {
+            first = node;
+            last = node;
+        } else { // want new node to point to the first node
+            node.next = first;
+            first = node;
+        }
+    }
+
+    /**
+     * Next, we are going to create the indexOf() method.
+     */
+    public int indexOf(int item) {
+        /*
+        To find the index, we need a node to hold the current index, and a node to
+        hold the index of the elements in general.
+         */
+        int index = 0;
+        var current = first;
+
+        while (current != null) {
+            /*
+            While current is not empty, if the current value is equal to the
+            item we are on, return that index.
+
+            Else, current = the next node, and increment its index.
+             */
+            if (current.value == item) {
+                return index;
+            } else {
+                current = current.next;
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Now we are going to create the contains() method.
+     * <p>
+     * An index can never be -1. so we return true if the
+     * index isn't negative.
+     */
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    /**
+     * Now we are going to create the deleteFirst() method.
+     * <p>
+     * Because of how LL work, in order for us to delete a node, we need
+     * a variable pointing to both the first node and the second node.
+     */
+    public void deleteFirst() {
+        // check if list is not empty.
+        if (isNotEmpty()) {
+
+
+            // when the list only has one item
+            if (first == last) {
+                first = last = null;
+                return;
+            }
+
+            // first we set second to = the next node after first
+            var second = first.next;
+
+            // then we set the first node to equal null to remove the element
+            first.next = null;
+
+            // then we set first to equal that second node
+            first = second;
+        }
+    }
+
+    /**
+     * Now we are going to create the deleteLast() method.
+     */
+    public void deleteLast() {
+        // when the list only has one item
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+
+        // first we need to get the previous node
+        var previous = getPreviousNode(last);
+        last = previous;
+        last.next = null;
+    }
+
+    // can use a method to see if the list is empty
+    private boolean isListEmpty() {
+        return first == null;
+    }
+
+    // can use a method to see if the list is not empty
+    private boolean isNotEmpty() {
+        return first != null;
+    }
+
+    private Node getPreviousNode(Node node) {
+        var current = first;
+        while (current != null) {
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    // the node class is a part of the linked list class
+    // we put it inside so that we can have access to its fields
+    private static class Node {
+        private int value;
+        private Node next;
+
+        // constructor that always gives the node a value.
+        public Node(int value) {
+            this.value = value;
         }
     }
 }
--- src/datastructures/linkedlists/Main.java
+++ src/datastructures/linkedlists/Main.java
@@ -3,7 +3,37 @@ package datastructures.linkedlists;
 public class Main {
     public static void main(String[] args) {
         /*
-        Here we are going to work with a Linked List class that we create on our own.
+        Here we are going to work with a Linked List class that we
+        created on our own using all the methods we created.
+
+        First we work with the addLast(), addFirst(), and indexOf() methods.
          */
+
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(60);
+        list.addLast(30);
+        list.addLast(90);
+        list.addFirst(45);
+        list.addFirst(55);
+        list.addFirst(78);
+        System.out.println(list.indexOf(90)); // returns 6
+        System.out.println(list.indexOf(60)); // returns 4
+
+        /*
+        When creating your own LinkedList class, you can only use methods that
+        you have created in the LinkedList class that you create.
+
+        So at this moment, we only have those three methods before we move on...
+         */
+
+        // checking to see if the list contains these numbers...
+        System.out.println(list.contains(78));
+        System.out.println(list.contains(30));
+        System.out.println(list.contains(10));
+
+        // Now we are finally going to check our delete methods
+        list.deleteFirst();
+        list.deleteLast();
     }
 }
