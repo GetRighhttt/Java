bbf980282d6a5263e92a60f41fd57e4c49ca5eb1
stefan <stefanbayne@gmail.com>
2022-08-18 11:27:05 -0400

    Objects explained.

---
+++ src/ObjectOrientedProgramming/WhatAreObjects.java
@@ -0,0 +1,38 @@
+package ObjectOrientedProgramming;
+
+import java.util.Locale;
+
+public class WhatAreObjects {
+    public static void main(String[] args) {
+        /*
+        An object is anything in the real world; essentially, it's an entity.
+        Car, dog, letter, name, person, television, computer, etc.
+
+        A class is a blueprint or template for creating objects.
+
+        The string class is a class and the names are the objects of that class.
+
+        The "new" keyword in java signifies an object.
+         */
+        String name = new String("Stefan");
+        String name1 = "John";
+        String name2 = "Vince";
+
+        // returns the length
+        System.out.println(name.length());
+
+        // repeats the name a number of times.
+        System.out.println(name1.repeat(4));
+
+        // makes all the letters uppercase
+        System.out.println(name2.toUpperCase(Locale.ROOT));
+
+        /**
+         * Output:
+         *
+         * 6
+         * JohnJohnJohnJohn
+         * VINCE
+         */
+    }
+}
