be908f1b2d54565fdabe1899396cffdc4bad2306
stefan <stefanbayne@gmail.com>
2022-08-18 14:32:14 -0400

    Chaining Functions.

--- src/functionalprogramming/_FunctionsExample.java
+++ src/functionalprogramming/_FunctionsExample.java
@@ -3,9 +3,6 @@ package functionalprogramming;
 import java.util.function.Function;
 
 public class _FunctionsExample {
-    // functional approach
-    static Function<String, String> returnTheString = name -> name;
-
     public static void main(String[] args) {
         /*
         Here we show an example of how to use functional programming with a
@@ -15,13 +12,26 @@ public class _FunctionsExample {
         // this is how we usually call functions in java.
         System.out.println(decrement(50)); // returns 49
 
-        // this is the functional approach using the function interface
-        Function<Integer, Integer> incrementByOne = x -> x++;
-
         // now we can call the String Function method like this
         String names = returnTheString.apply("Stefan Bayne");
         System.out.println(names);
+
+        // this is the functional approach using the function interface
+        Function<Integer, Integer> incrementByOne = x -> x + 1;
+
+        // here we will create a multiply method to show how to chain functions
+        Function<Integer, Integer> multiplyByOne = x -> x * 50;
+
+        Function<Integer, Integer> andOneMultiplyByOne =
+                incrementByOne.andThen(multiplyByOne);
+        System.out.println(andOneMultiplyByOne.apply(10)); // returns 550
+
+        System.out.println(andOneMultiplyByOne.apply(46)); // returns 2350
+
     }
+
+    // functional approach
+    static Function<String, String> returnTheString = name -> name;
 
     // typical approach aka Imperative approach
     static int decrement(int a) {
