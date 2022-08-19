3230af79299a2bde7d549f77f9b4656cd84dfa84
stefan <stefanbayne@gmail.com>
2022-08-18 20:02:17 -0400

    Added a list for suppliers.

--- src/functionalprogramming/_Suppliers.java
+++ src/functionalprogramming/_Suppliers.java
@@ -1,5 +1,6 @@
 package functionalprogramming;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class _Suppliers {
@@ -12,8 +13,18 @@ public class _Suppliers {
      */
     static Supplier<Integer> getNumber = () -> 50;
     static Supplier<String> getName = () -> "Stefan";
+    static Supplier<List<Integer>> getNewNumbers =
+            () -> List.of(20, 30, 40, 50, 60);
+
+
     public static void main(String[] args) {
+
+        /*
+        Now let's get the information:
+         */
+
         System.out.println(getNumber.get()); // returns 50
         System.out.println(getName.get()); // returns "Stefan"
+        System.out.println(getNewNumbers.get()); // returns [20, 30, 40, 50, 60]
     }
 }
