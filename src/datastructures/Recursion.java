2aa16ad77851bcd9bae8b1d15539984dde534efb
stefan <stefanbayne@gmail.com>
2022-09-13 13:25:40 -0400

    Added factorial recursion.

--- src/datastructures/Recursion.java
+++ src/datastructures/Recursion.java
@@ -14,15 +14,27 @@ public class Recursion {
 
         // writing to the console
         System.out.println(SumDigits(arr.length - 1));
+        System.out.println(FactorialDigits(arr.length));
     }
 
     // Method to sum all the digits in the array recursively.
-    public static int SumDigits(int n) {
+    public static int SumDigits(int n) { // = 27
 
         // if index is 0, return the array
         if (n == 0) return arr[n];
 
         // return the array of elements and sum them as the index decreases by 1
         return arr[n] + SumDigits(n - 1);
+    }
+
+    public static int FactorialDigits(int n) { // = 5040
+
+        // if index is 1, return the array
+        if (n == 1) {
+            return 1;
+        } else { // else return n * factorial(n - 1)
+            return n * FactorialDigits(n - 1);
+        }
+
     }
 }
